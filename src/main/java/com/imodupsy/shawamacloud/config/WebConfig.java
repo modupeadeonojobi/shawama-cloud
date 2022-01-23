fa8ef52baf5c526e206929194d4a4629dd3eeef5
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-01-23 17:34:03 +0100

    Add webconfig for home template

---
+++ src/main/java/com/imodupsy/shawamacloud/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.imodupsy.shawamacloud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author iModupsy
+ * @created 23/01/2022
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
