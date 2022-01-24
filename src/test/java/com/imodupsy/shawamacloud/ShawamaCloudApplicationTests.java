0481abe501fabb73992d6f26a79009e4241348ca
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-01-24 09:23:32 +0100

    Remove homeController and use webConfig to define the root path "/"

--- src/test/java/com/imodupsy/shawamacloud/ShawamaCloudApplicationTests.java
+++ src/test/java/com/imodupsy/shawamacloud/ShawamaCloudApplicationTests.java
@@ -1,6 +1,6 @@
 package com.imodupsy.shawamacloud;
 
-import com.imodupsy.shawamacloud.controller.HomeController;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 class ShawamaCloudApplicationTests {
 
   @Autowired
