ee10e4ba42f8d6ec3ff0bbfbadfd17b4ca935e03
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-01-30 04:02:46 +0100

    Add Oauth2; social login with facebook

---
+++ src/main/java/com/imodupsy/shawamacloud/enums/Provider.java
@@ -0,0 +1,9 @@
+package com.imodupsy.shawamacloud.enums;
+
+/**
+ * @author iModupsy
+ * @created 30/01/2022
+ */
+public enum Provider {
+    LOCAL, FACEBOOK
+}
--- src/main/java/com/imodupsy/shawamacloud/security/SecurityConfig.java
+++ src/main/java/com/imodupsy/shawamacloud/security/SecurityConfig.java
@@ -1,12 +1,8 @@
 package com.imodupsy.shawamacloud.security;
 
-import com.imodupsy.shawamacloud.entity.User;
-import com.imodupsy.shawamacloud.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,8 +24,24 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
-                .antMatchers("/design", "/orders").hasRole("USER")
-                .antMatchers("/", "/**").permitAll()
+                    .antMatchers("/design", "/orders").hasRole("USER")
+                    .antMatchers("/", "/**").permitAll()
+
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+                // The route you want to navigate the users to immediately they are logged in.
+                        .defaultSuccessUrl("/design", true)
+                        .loginProcessingUrl("/authenticate")
+                        .usernameParameter("/user")
+                        .passwordParameter("/password")
+
+                .and()
+                    .oauth2Login()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/design", true)
+
+
                 .and()
                 .build();
     }
